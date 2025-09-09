68d39f9c5b9f4997ca4ac1966be6494e558d6a82
Wolfyxon <wolfyxon@gmail.com>
2025-09-09 15:49:26 +0200

    Implemented main params

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
+__attribute__((naked))
 void _start(void) {
-    // TODO: Get argc, argv and call main()
-
-    int argc = 0;
-    char** argv;
-    char** envp;
+    register long *rsp asm("rsp");
+    
+    int argc = (int) rsp[0];
+    char** argv = (char**) &rsp[1];
+    char** envp = (char**) &rsp[1 + argc + 1];
 
     int code = main(argc, argv, envp);
     exit(code);
